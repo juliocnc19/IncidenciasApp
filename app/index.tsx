97bbc540f0c8a0fae6f7cf36253cab8bcf844398
juliocnc19 <juliocnietoc19@gmail.com>
2025-04-22 15:36:23 -0400

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "../global.css"
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Prueba de Mensaje</Text>
+    </View>
+  )
+}
